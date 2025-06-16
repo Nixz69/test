34ee43ded78271c1a89af5dc75a239fb9dc2bf8a
Nixz69 <noprimeakk01@gmail.com>
2025-06-16 12:16:44 +0700

    saqwe

--- my/src/hooks.ts
+++
@@ -1,7 +0,0 @@
-// src/hooks.ts
-import { useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from './store';
-import type { TypedUseSelectorHook } from 'react-redux';
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
